fe290882e72b611b041e6b00a276fc6db6b1a04f
Eric Fode <Eric@heroku.com>
2013-03-22 15:10:40 -0700

    Added more documentation to the interfaces.go file

--- piping/interfaces.go
+++ piping/interfaces.go
@@ -4,11 +4,16 @@ import (
 	"l2met/store"
 )
 
+//All async parts of the application implement this
+//While it may be seen as overly abstract we are all terrible at concurrnecy
+//and it should be kept in nice neat boxes.
 type Task interface {
 	Start()
 	Stop()
 }
 
+//A partitioner is used to help with synchronisation between multiple parts
+//of the application, currently it is used between the redis source and outlet
 type Partitioner interface {
 	//Get a Lock on a partition 
 	LockPartition() uint64
@@ -27,7 +32,8 @@ type Sender interface {
 	GetSenderChannel() chan *store.Bucket
 }
 
-//A Receiver is responsible for holding on to  
+//A Receiver is responsible for holding on to the incomming buckets in a givin
+//componenet until it can get to it.
 type Receiver interface {
 	SetInput(input chan *store.Bucket)
 	//Channel that feeds out of the reciver into your application logic
@@ -35,22 +41,24 @@ type Receiver interface {
 }
 
 //A component that both recives and sends.
-//An example would be a partitioner (takes a stream of objects and devides them up for work)
+//An example would be a demultiplexer
+//(takes a stream of objects and devides them up for work)
 type Aggregator interface {
 	Task
 	Receiver
 	Sender
 }
 
-//An outlet to something out side of the application
-//PG_outlet is an example
+//An outlet to something outside of the application
+//PostgresOutlet is an example (which puts buckets into postgres)
+//RedisOutlet is an example (which puts buckets into redis for distrubution)
 type Outlet interface {
 	Task
 	Receiver
 }
 
-//A source creates buckets and partitions them
-//to all of the Outlets registered with it.
+//A source creates buckets and and puts them into it's sender channel(s)
+//Examples would include RedisSource and later HTTPSource and PostgresSource
 type Source interface {
 	Task
 	Sender
